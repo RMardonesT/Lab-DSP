2e1c5954da47da789b7493bf764f3bc017b9db98
RodrigoGraves <38192646+RodrigoGraves@users.noreply.github.com>
2021-04-10 19:49:23 -0400

    codigos p1, p2, p3
    Avances en p3

--- lab1dsp_GN_Graves_Mardones/labP1/scripts/p3.m
+++ lab1dsp_GN_Graves_Mardones/labP1/scripts/p3.m
@@ -1,8 +1,12 @@
 %% Generación de señales
 close all; clear; clc;
+
 %1
-fs = 2000  ; %2kHz, sobre frecuencia de nyquist
-N  = 2000*1; %fs * Tiempo de duración
+%  Se crean s50 y s500 (senos de 50 y 500 Hz) y se muestrean a 2500Hz,
+%luego se grafican con respecto al tiempo y se escuchan.
+
+fs = 4000; %4kHz, sobre frecuencia de nyquist
+N  = fs*1; %fs * Tiempo de duración
 n  = 0:N-1 ; %Vector de muestras
 
 s50  = sin(2*pi*(50/fs)*n) ; %señal de 50Hz muestreada a fs
@@ -16,5 +20,32 @@ subplot 212
 plot(n/fs,s500); xlim([0 0.1]);
 xlabel("tiempo s");ylabel("Amplitud");title("Señal s_{500}(t) vs tiempo")
 
-soundsc(s50,fs);
-soundsc(s500,fs);
+%soundsc(s50,fs);
+%soundsc(s500,fs);
+
+%2
+%  Se generan y se escuchan:
+%     - s21: suma de seno de 50 y 500 Hz
+%     - s22: suma de seno de 200 y 300 Hz
+%     - A  : Acorde La mayor, suma de seno de 880,1100 y 1320 Hz
+s21 = s50+s500;
+figure(2)
+subplot 311
+plot (n/fs,s21); xlim([0 0.05]);
+xlabel("tiempo s");ylabel("Amplitud");
+title("s_{50}(t) + s_{500}(t)  vs tiempo")
+%soundsc(s21,fs);
+
+subplot 312
+s22 = sin(2*pi*(200/fs)*n) +sin(2*pi*(300/fs)*n);
+plot (n/fs,s22); xlim([0 0.05]);
+xlabel("tiempo s");ylabel("Amplitud");
+title("s_{200}(t) + s_{300}(t)  vs tiempo")
+%soundsc(s22,fs);
+
+subplot 313
+A = sin(2*pi*(880/fs)*n) +sin(2*pi*(1100/fs)*n)+sin(2*pi*(1320/fs)*n);
+plot (n/fs,A); xlim([0 0.05]);
+xlabel("tiempo s");ylabel("Amplitud");
+title("s_{880}(t) + s_{1100}(t) + s_{1320}(t) vs tiempo")
+%soundsc(A,fs);
