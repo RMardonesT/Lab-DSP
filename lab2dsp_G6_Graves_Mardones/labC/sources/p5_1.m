a59e0449c6d7ed4181ef7f9b3f860badf4969ef2
RMardonesT <ricardo.mardones@sansano.usm.cl>
2021-05-13 23:49:27 -0400

    aliasing

--- lab2dsp_G6_Graves_Mardones/labC/sources/p5_1.m
+++ lab2dsp_G6_Graves_Mardones/labC/sources/p5_1.m
@@ -27,4 +27,57 @@ title("Señal aleatoria de 10 muestras generada y su interpolación")
 legend("Muestras interpoladas sin retardo de grupo", "Muestras se la señal original")
 
 %% aliasing_test P = 3
+clear all; close all; clc;
 
+P=3;
+load("aliasing_test_16_16.mat");
+
+aliasing = aliasing_test';
+
+%% Upsampling
+upsampled = [];
+N1 = length(aliasing);
+
+for i = 1:N1
+     upsampled = horzcat(upsampled, aliasing(i), zeros(1,P-1));
+end
+%% Filtrado
+
+B = fir1(40, 1/P);
+filtered =  filter(B,1,upsampled);
+
+%%
+mag1 = abs(fftshift(fft(aliasing)));
+w1 = (-N1/2:N1/2-1)/(N1*2*pi);
+
+mag2 = abs(fftshift(fft(upsampled)));
+N2 = length(upsampled);
+w2 = (-N2/2:N2/2-1)/N2*2*pi;
+
+mag3 =  abs(fftshift(fft(filtered)));
+N3 = length(filtered);
+w3 = (-N3/2:N3/2-1)/N3*2*pi;
+
+%% Graficas de magnitud
+
+
+subplot(3,1,1)
+plot(w1,mag1);
+xlim([-pi,pi])
+xlabel("Frecuencia normalizada [rad/muestra]")
+ylabel("Magnitud [-]")
+title("Señal original")
+
+subplot(3,1,2)
+plot(w2,mag2);
+xlim([-pi,pi])
+xlabel("Frecuencia normalizada [rad/muestra]")
+ylabel("Magnitud [-]")
+title("Señal luego de sufrir upsampling")
+
+subplot(3,1,3);
+plot(w3,mag3)
+xlim([-pi,pi])
+xlabel("Frecuencia normalizada [rad/muestra]")
+ylabel("Magnitud [-]")
+title("Resultado luego de sufrir upsampling y filtrado")
