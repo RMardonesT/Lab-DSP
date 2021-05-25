d20d7ac56fa4f79d381a29b9d02fe462dccfdd93
RMardonesT <ricardo.mardones@sansano.usm.cl>
2021-05-25 15:33:29 -0400

    parametros notch

--- lab3dsp_G6_Graves_Mardones/labC/sources/s-function_notch/notch.c
+++ lab3dsp_G6_Graves_Mardones/labC/sources/s-function_notch/notch.c
@@ -42,7 +42,16 @@ typedef struct bqState_t {
 /*---------------------------------------------------------------------------*/
 /* VARABLES FILTRO NOTCH */
 /*---------------------------------------------------------------------------*/
+bqState_t Notch440 = {
+                      -1.932273671,  // a1
+                      0.9614814516, // a2
+                      0.9807407258, //b0
+                      -1.932273671, //b1
+                      0.9807407258,  //b2
 
+                      {0 ,0 ,0}, //Inputs buffer
+                      {0,0,0} //Outputs buffer
+                      }
 
 
 
@@ -51,6 +60,30 @@ typedef struct bqState_t {
 ******************************************************************************/
 
 static double filterBiquad(bqState_t *filterNState, double filterInput);
+
+extern double notch(double);
+
+
+
+/******************************************************************************
+**      FUNCTION DEFINITIONS
+******************************************************************************/
+
+extern double notch(double data)
+{
+  return filterBiquad(&Notch440 , u1);
+}
+
+/******************************************************************************
+*   \brief  Esta función implementa una etapa de filtro biquad
+*
+*   \param filterNState     : puntero a la estructura del biquad a ejecutar
+*   \param filterInput          : señal de entrada al filtro biquad a ejecutar
+*
+*   \return filterOutput        : señal de salida del filtro biquad ejecutado
+******************************************************************************/
+static double filterBiquad(bqState_t *filterNState, double filterInput)
+{
   //Desplazamiento de datos en la linea de retardo de tamaño 3
   filterNState->bqInput[2] = filterInput->bqInput[1];
   filterNState->bqInput[1] = filterInput->bqInput[0];
@@ -68,31 +101,6 @@ static double filterBiquad(bqState_t *filterNState, double filterInput);
 
   filterNState->bqOutput[0] = y;
   return y;
-
-extern double notch(double);
-
-
-
-/******************************************************************************
-**      FUNCTION DEFINITIONS
-******************************************************************************/
-
-extern double notch(double data)
-{
-
-
-}
-
-/******************************************************************************
-*   \brief  Esta función implementa una etapa de filtro biquad
-*
-*   \param filterNState     : puntero a la estructura del biquad a ejecutar
-*   \param filterInput          : señal de entrada al filtro biquad a ejecutar
-*
-*   \return filterOutput        : señal de salida del filtro biquad ejecutado
-******************************************************************************/
-static double filterBiquad(bqState_t *filterNState, double filterInput)
-{
 
 
 }
