da6a5846fff3f05239bb10fca9e1f2013e2f57ec
RodrigoGraves <38192646+RodrigoGraves@users.noreply.github.com>
2021-06-01 16:35:01 -0400

    lab4 p3
    p3

---
+++ lab4dsp_G6_Graves_Mardones/labP1/scripts/p2.m
@@ -0,0 +1,53 @@
+%%%%% Parte II Lab 4 DSP
+% ¡ACTUALIZAR n! en linea 8 y 43 usando wgn()
+close all; clear; clc
+
+Fs = 2000; Ts = 1/Fs; t = 0:Ts:0.1-Ts; N = length(t);
+
+% ruido, actualizar a gaussiano
+n = sqrt(1/0.0909)*(rand(1,N)-.5); %wgn(1,length(t),)
+var(n)
+
+% señal limpia
+s = .5*cos(2*pi*100*t) + 1.5*sin(2*pi*500*t);
+
+% señal con ruido
+sn = s + n;
+
+%%% p21: Gráfico en el tiempo 
+figure
+plot(t,sn);
+xlabel("tiempo [s]");ylabel("Amplitud [-]");
+title("2 tonos puros con ruido blanco gaussiano de variaza 2 y media 0")
+
+%%% p22: FFT y gráficos de amplitud en frecuencia
+SN = fft(sn,N);
+f  = (0:(N-1))*Fs/N;
+figure
+stem(f, abs(SN));xlim([0 1000]);
+xlabel("Frecuencia [Hz]");ylabel("Amplitud [-]");
+title("Contenido en frecuencia de señal ruidosa");
+
+%%%p23: Gráfico en dB para contenido en frecuencia 
+%100 ms
+SN_dB = 20*log10(abs(SN));
+figure
+subplot(2,1,1)
+plot(f, SN_dB - max(SN_dB));xlim([0 1000]);
+xlabel("Frecuencia [Hz]");ylabel("Amplitud [dB]");
+title("Contenido en frecuencia de señal ruidosa en dB (0.1s)");
+
+%1s
+Fs = 2000; Ts = 1/Fs; t = 0:Ts:1-Ts; N = length(t);
+
+n = sqrt(1/0.0909)*(rand(1,N)-.5); var(n)     % rudio wgn(1,length(t),)
+s = .5*cos(2*pi*100*t) + 1.5*sin(2*pi*500*t); % señal limpia
+sn = s + n;                                   % señal con ruido
+
+SN_dB = 20*log10(abs(fft(sn,N)));
+f  = (0:(N-1))*Fs/N;
+
+subplot(2,1,2)
+plot(f, SN_dB - max(SN_dB));xlim([0 1000]);
+xlabel("Frecuencia [Hz]");ylabel("Amplitud [dB]");
+title("Contenido en frecuencia de señal ruidosa en dB (1s)");
