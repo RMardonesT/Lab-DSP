bd35ead22bcadd69985bc5f59b4e5b5c3e4f9641
RodrigoGraves <38192646+RodrigoGraves@users.noreply.github.com>
2021-06-04 02:12:08 -0400

    lab 3 iir
    iir

--- lab3dsp_G6_Graves_Mardones/labP1/scripts/IIR_filter.m
+++ lab3dsp_G6_Graves_Mardones/labP1/scripts/IIR_filter.m
@@ -3,7 +3,7 @@ function nextyBuff = IIR_filter(yBuff, aCoeff, x)
     a2 = aCoeff(2);
     
     %Filtrado por ecuación de diferencias
-    output = (a1*yBuff(1) + a2*yBuff(2) + x);
+    output = (- a1*yBuff(1) - a2*yBuff(2) + x);
     
     %Actualización del Buffer para siguiente señal de entrada
     yBuff(2) = yBuff(1);
