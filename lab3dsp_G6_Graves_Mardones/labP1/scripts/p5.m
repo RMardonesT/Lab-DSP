9ca42c4fb029ca4c1f855c35802b2dff67d9e79b
RMardonesT <ricardo.mardones@sansano.usm.cl>
2021-06-04 22:47:25 -0400

    Update p5.m

--- lab3dsp_G6_Graves_Mardones/labP1/scripts/p5.m
+++ lab3dsp_G6_Graves_Mardones/labP1/scripts/p5.m
@@ -7,9 +7,19 @@ fsp = 4000; Wps = fsp/fs;
 Rp = 0.5;
 Rs = 40;
 
-[n1,w1] = buttord(Wp,Wps,Rp,Rs);
+[n1,w1] = buttord(Wp,Wps,Rp,Rs);  %Butterworth IIR
 
-[n2,w2] = cheb1ord(Wp,Wps,Rp,Rs);
+[n2,w2] = cheb1ord(Wp,Wps,Rp,Rs); %Chebyshev IIR
 
-[n3,w3] = ellipord(Wp,Wps,Rp,Rs);
+[n3,w3] = ellipord(Wp,Wps,Rp,Rs); %Elliptic IIR
+
+
+%https://la.mathworks.com/help/signal/ref/firpmord.html
+% Parks-McClellan
+
+f = [fp fsp];
+a  = [1 0];
+dev = [(10^(Rp/20)-1)/(10^(Rp/20)+1)  10^(-Rs/20)];
+
+[n4,fo,ao,w] = firpmord(f,a,dev,fs);  %%FIR
 
