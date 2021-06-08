e379b5845521089a00e3a59d34b723edd4d68ab9
RMardonesT <ricardo.mardones@sansano.usm.cl>
2021-06-08 16:23:57 -0400

    5.1

---
+++ lab4dsp_G6_Graves_Mardones/labP1/scripts/genAmatrix.m
@@ -0,0 +1,11 @@
+function A = genAmatrix(N)
+    A = zeros(N,N);
+    
+    for k =1:N
+        for  n = 1:N
+            A(k,n) = exp(-1j*2*pi*(k-1)*(n-1)/N);
+        end
+        
+    end
+    
+end
