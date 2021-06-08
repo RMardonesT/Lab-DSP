e7f0cbb4ed4d5abf0764ca36174bd6b36ab76768
RodrigoGraves <38192646+RodrigoGraves@users.noreply.github.com>
2021-06-08 16:02:35 -0400

    p4_3 lab 4
    p4_3

---
+++ lab4dsp_G6_Graves_Mardones/labP1/scripts/p4_3.m
@@ -0,0 +1,22 @@
+%%%%% Parte IV Lab 4
+close all; clear; clc;
+
+%%% 3) Comparación DFTsum() con fft() para 500 puntos
+Fs = 5000; N = 5000; t = (0:N-1)/Fs;
+
+x = cos(2*pi*100*t); % Señal a analizar
+
+% Obtención de espectros y error espectral
+f = (0:N-1)*Fs/N;
+X_DFTsum = DFTsum(x); 
+X_fft    = fft(x,N);
+
+e   = X_DFTsum - X_fft;
+
+format long
+X_mse = sum(abs(e).^2)/N 
+
+%Gráficos
+plot(f,abs(e));
+title("Magnitud del Error del espectro, calculado con DFTsum y fft")
+xlabel("frecuencia [Hz]"); ylabel("Amplitud [-]");
