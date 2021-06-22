1c3f33270e7b7b7fc4396ae62ba167a90a99bb77
RMardonesT <ricardo.mardones@sansano.usm.cl>
2021-06-22 16:36:37 -0400

    p1_1

---
+++ lab5dsp_G6_Graves_Mardones/labP1/scripts/p1_1.m
@@ -0,0 +1,19 @@
+clear all; clc;
+
+%% 1.1
+
+fs = 8000;
+t =1;
+f = 100;
+T = fs/f;
+y = exciteV(t*fs,T);
+
+
+
+Y =   fft(y);
+Ymod = abs(Y)
+Ydb = 20*log10( 0.000000001 + Ymod(1:fs/2))  %%se incrementan los valores en una cantidad despreciable para evitar el log10(0)
+plot([0:fs/2-1],  Ydb(1:fs/2))
+
+
+
